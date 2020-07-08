0d4dea602d442772e7a9a6c51274f8e0e0f4a421
Adam <harageth@gmail.com>
2020-07-08 10:31:18 -0700

    initial commit with spring initializr

---
+++ src/test/java/com/harageth/battletech/battletechSearch/BattletechSearchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.harageth.battletech.battletechSearch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BattletechSearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
